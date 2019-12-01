8a8eefcb929362d5d361ee52d0c4476d3a167224
thamidurm <thamidurm@gmail.com>
2019-12-01 16:29:22 +0530

    remove code for static asset serving

--- src/app.js
+++ src/app.js
@@ -13,7 +13,6 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
